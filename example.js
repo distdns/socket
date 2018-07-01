100dc2da0024195c29f2a27b94570a99411a0cca
Jared Rice Sr <jaredricelegal@gmail.com>
2018-07-01 17:45:05 -0500

    Pushed first public verson of Distributed DNS Socket

---
+++ example.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const dns = require('.')
+const socket = dns()
+
+socket.query({
+  flags: dns.RECURSION_DESIRED,
+  questions: [{
+    type: 'ANY',
+    name: 'www.dr.dk'
+  }]
+}, 53, '8.8.8.8', function (err, response) {
+  console.log(err, response)
+  socket.destroy()
+})
